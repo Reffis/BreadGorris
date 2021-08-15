0340492c589a7e63bfaadb54cb100c365974831f
fn79 <color0470@gmail.com>
2021-08-15 15:03:24 +0900

    Update `colors.go`

---
+++ src/colors.go
@@ -0,0 +1,21 @@
+package src
+
+const (
+	RED = 0xff1f1f
+
+	ORANGE = 0xffa21f
+
+	YELLOW = 0xffff1f
+
+	GREEN = 0x41ff1f
+
+	BLUE = 0x1fdaff
+
+	INDIGO = 0x391fff
+
+	PURPLE = 0xce1fff
+
+	BLACK = 0x000000
+
+	WHITE = 0xffffff
+)
