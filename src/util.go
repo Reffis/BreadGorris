0eb25d9a1997f0b60dfa4f6994310ebe7caad273
fn79 <color0470@gmail.com>
2021-08-15 23:07:54 +0900

    Update `help` Command

--- src/util.go
+++ src/util.go
@@ -10,7 +10,7 @@ func Help(s *discordgo.Session, m *discordgo.MessageCreate, _ []string) CmdResul
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 		Title:       "도움말",
 		Description: "**Prefix**: `gorris`",
-		Color:       WHITE,
+		Color:       RandomColors(),
 		Footer:      &discordgo.MessageEmbedFooter{Text: m.Author.Username, IconURL: m.Author.AvatarURL("1024")},
 		Fields: []*discordgo.MessageEmbedField{
 			{Name: "Utility", Value: UTIL_TEXT, Inline: true},
