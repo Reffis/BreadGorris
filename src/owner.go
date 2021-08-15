cbf600e8137430104008d3d838d9a0cfd341ec44
fn79 <color0470@gmail.com>
2021-08-15 17:23:18 +0900

    Add `status` Command

--- src/owner.go
+++ src/owner.go
@@ -1,3 +1,24 @@
 package src
 
-const OWNER_TEXT = "> "
+import (
+	"errors"
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const OWNER_TEXT = "> `status [Text]`"
+
+func Status(s *discordgo.Session, m *discordgo.MessageCreate, args []string) CmdResult {
+	if m.Author.ID != owner {
+		return CmdResult{"status", errors.New("You are not the owner of this bot.")}
+	}
+	items := args[2:]
+	text := " "
+	for _, item := range items {
+		text += item + " "
+	}
+	s.UpdateStatus(0, text)
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintln("Status updated: `", text, "`"))
+	return CmdResult{"status", nil}
+}
